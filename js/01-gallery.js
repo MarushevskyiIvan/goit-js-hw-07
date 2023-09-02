928725658c4f56cab14966244f1006ee7919370e
Marushevskyi Ivan <131617971+MarushevskyiIvan@users.noreply.github.com>
2023-09-03 00:05:54 +0300

    7.1

---
+++ js/01-gallery.js
@@ -0,0 +1,57 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+// console.log(galleryItems);
+
+const imageGalleryEl = document.querySelector(".gallery");
+
+imageGalleryEl.addEventListener("click", onImgClickModal);
+
+const galleryItemEl = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img 
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    /></a></li>`
+  )
+  .join("");
+
+imageGalleryEl.innerHTML = galleryItemEl;
+
+function onImgClickModal(event) {
+  event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return;
+  }
+
+  const targetEl = event.target.closest(".gallery__image");
+  const imageDataEl = targetEl.dataset.source;
+
+  const imageDataInfo = galleryItems.find(
+    (data) => data.original === imageDataEl
+  );
+
+  const instance = basicLightbox.create(`
+      <div class="modal">
+      <img
+      src="${imageDataInfo.original}"
+      alt="${imageDataInfo.description}"
+    />
+      </div>
+  `);
+
+  instance.show(window.addEventListener("keydown", onCloseModalFromEsc));
+
+  function onCloseModalFromEsc(event) {
+    if (event.code === "Escape") {
+      instance.close(
+        window.removeEventListener("keydown", onCloseModalFromEsc)
+      );
+    }
+  }
+}
